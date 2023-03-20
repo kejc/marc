423509519a5feddd13993e72caaa4d064cb8b7d4
Kenneth Clausen <me@kjc.dk>
2023-03-20 14:30:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+const vandcykel = ref('');
+
+function onSubmit() {
+  alert(vandcykel.value);
+}
+</script>
+
+<template>
+  <input type="text" v-model="vandcykel" />
+  <button v-on:click="onSubmit">
+    Dette er en knap
+  </button>
+  {{ vandcykel }}
+</template>
